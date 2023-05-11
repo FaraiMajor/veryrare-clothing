239e6258a3c0edaf3136162c45ce498b087fca98
Farai Major <faraimutukumira@gmail.com>
2023-05-11 02:18:31 -0400

    worked on routes for basic site structure we have so far using react router

---
+++ src/routes/navigation/navigation.jsx
@@ -0,0 +1,29 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import Diamond from '../../assets/diamond-svg.png'
+
+import './navigation.scss';
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                <Link className='logo-container' to='/'>
+                    <img className='logo' src={Diamond} />
+                    <h1 className='logo-text'>VERYRARE</h1>
+                </Link>
+                <div className='nav-links-container'>
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
+                    </Link>
+                    <Link className='nav-link' to='/sign-in'>
+                        SIGN IN
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    );
+};
+
+export default Navigation;
