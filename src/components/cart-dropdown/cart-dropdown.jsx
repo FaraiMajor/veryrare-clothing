14c4b01d1654e9a8685d32993be09980ba40b4e6
Farai Major <faraimutukumira@gmail.com>
2023-05-27 09:52:24 -0400

    cart dropdowm done

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button';
+import CartItem from '../cart-item/cart-item';
 
 import './cart-dropdown.scss';
 
-const CartDropdown = () => (
-    <div className='cart-dropdown-container'>
-        <div className='cart-items' />
-        <Button>View Bag & Checkout</Button>
-    </div>
-);
+const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
+
+    return (
+        <div className='cart-dropdown-container'>
+            <div className='cart-items'>
+                {cartItems.length ? (
+                    cartItems.map((cartItem) => (
+                        <CartItem key={cartItem.id} cartItem={cartItem} />
+                    ))
+                ) : (
+                    <span className='empty-message'>Your cart is empty</span>
+                )}
+            </div>
+            <Button>GO TO CHECKOUT</Button>
+        </div>
+    );
+};
 
 export default CartDropdown;
