804259cd2d43c1d7301ab081494056366935cc27
Farai Major <faraimutukumira@gmail.com>
2023-06-08 02:43:56 -0400

    fix grid

--- src/components/category-preview/category-preview.styles.jsx
+++ src/components/category-preview/category-preview.styles.jsx
@@ -38,16 +38,13 @@ export const Preview = styled.div`
   column-gap: 20px;
 
   @media screen and (max-width: 800px) {
-    grid-template-columns: 1fr;
+    grid-template-columns: 1fr 1fr;
     grid-column-gap: 15px;
-    grid-row-gap: 25px;
-
-       
+    grid-row-gap: 25px;       
   }
 
   @media screen and (max-width: 400px) {
     grid-template-columns: 1fr;
     grid-row-gap: 45px;
-  
   }
 `;
--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -8,19 +8,15 @@ export const CategoryContainer = styled.div`
     margin: 0 15px 50px;
 
     @media screen and (max-width: 800px) {
-        // grid-template-columns: 1fr 1fr;
-        // grid-column-gap: 15px;
-        // grid-row-gap: 25px;
+        grid-template-columns: 1fr 1fr;
+        grid-column-gap: 15px;
+        grid-row-gap: 25px;
       }
     
       @media screen and (max-width: 400px) {
-        // grid-template-columns: 1fr;
-        // grid-row-gap: 45px;
-        // margin: 0 35px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
+        grid-template-columns: 1fr;
+        grid-row-gap: 45px;
+        margin: 0 35px;
       }
 `;
 
