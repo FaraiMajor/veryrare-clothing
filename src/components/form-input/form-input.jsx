6213a33cc1b6f3a89fb586ad9c4478c00fc88501
Farai Major <faraimutukumira@gmail.com>
2023-06-12 02:02:04 -0400

    fix sign up issue

--- src/components/form-input/form-input.jsx
+++ src/components/form-input/form-input.jsx
@@ -1,6 +1,6 @@
 import { FormInputLabel, Input, Group } from './form-input.styles';
 
-const FormInput = ({ label, ...inputOptions }) => {
+const FormInput = ({ label, inputOptions }) => {
     return (
         <Group>
             <Input {...inputOptions} />
